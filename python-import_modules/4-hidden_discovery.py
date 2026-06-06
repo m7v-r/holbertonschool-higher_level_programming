f910e261395456f45c37987d743e685b2010e77d
m7v-r <ms4571690@icloud.com>
2026-06-06 19:45:42 +0300

    Optimize task 4 for sandbox auto-setup

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import os
-    import subprocess
-
-    if not os.path.exists("/tmp/hidden_4.pyc"):
-        url = "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
-        subprocess.run(["curl", "-Lso", "/tmp/hidden_4.pyc", url])
-
-    import sys
-    sys.path.append('/tmp')
     import hidden_4
 
     all_names = dir(hidden_4)
@@ -17,4 +8,3 @@ if __name__ == "__main__":
     for name in all_names:
         if not name.startswith("__"):
             print("{}".format(name))
-
