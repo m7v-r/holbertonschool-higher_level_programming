9a956be55b9195e97629ece2aef9f71ae55c544c
m7v-r <ms4571690@icloud.com>
2026-06-13 18:28:23 +0300

    Fix pycodestyle style issues in Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,16 +7,19 @@ def roman_to_int(roman_string):
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
+
     total = 0
     length = len(roman_string)
-    
+
     for i in range(length):
         current_val = roman_dict.get(roman_string[i], 0)
-        
-        if i + 1 < length and current_val < roman_dict.get(roman_string[i + 1], 0):
+        next_val = 0
+        if i + 1 < length:
+            next_val = roman_dict.get(roman_string[i + 1], 0)
+
+        if current_val < next_val:
             total -= current_val
         else:
             total += current_val
-            
+
     return total
