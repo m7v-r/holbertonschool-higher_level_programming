4738e34c0f24b2353d875260ce9697226663e461
m7v-r <ms4571690@icloud.com>
2026-07-04 16:42:41 +0300

    Add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines the Student class with attribute filtering.
+"""
+
+
+class Student:
+    """
+    Representation of a student with filtering capabilities.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): List of attribute names to retrieve.
+
+        Returns:
+            dict: The dictionary representation.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
