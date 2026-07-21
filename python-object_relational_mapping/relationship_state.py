d09512fb0016f33798cfa0d38c3320ea51d82e34
m7v-r <ms4571690@icloud.com>
2026-07-21 14:24:16 +0300

    Add Task 15: City relationship

---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains the class definition of a State for relationship."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class links to the MySQL table states with cities relationship."""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
