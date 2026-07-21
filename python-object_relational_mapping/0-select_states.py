dc8571027d783c242f40ba2963daae3d4e5b2a86
m7v-r <ms4571690@icloud.com>
2026-07-21 14:06:20 +0300

    Fix Task 0 SQL query and formatting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,30 +4,18 @@ import sys
 import MySQLdb
 
 
-def main():
-"""Lists all states from the database hbtn_0e_0_usa."""
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
-
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=db_name
-)
-
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-cursor.close()
-db.close()
-
-
 if __name__ == "__main__":
-main()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
