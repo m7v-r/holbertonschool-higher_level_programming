c31db330a4cff6c9e9c764057ba6af988643620a
m7v-r <ms4571690@icloud.com>
2026-07-21 14:29:39 +0300

    Fix pep8 unused import and requirements for task 16

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Lists all State objects and corresponding City objects using relationship."""
+"""Lists all State objects and corresponding City objects."""
 import sys
-from relationship_city import City
 from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id.asc()).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
@@ -25,4 +24,3 @@ if __name__ == "__main__":
             print("\t{}: {}".format(city.id, city.name))
 
     session.close()
-
