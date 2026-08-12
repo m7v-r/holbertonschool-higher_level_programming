10ea0bd7c6cd40e58892251dba384dba0cf606e8
m7v-r <ms4571690@icloud.com>
2026-08-13 02:22:48 +0300

    Add task_01_jinja.py and Jinja HTML templates

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Flask application with Jinja templates for home, about, and contact pages.
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Renders the home page."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
