b5f93cbdec4a6a97807e550f7df4aa6eb4e81bfc
m7v-r <ms4571690@icloud.com>
2026-08-13 02:12:02 +0300

    Add 6-script.js for task 6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,8 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+const characterElement = document.querySelector('#character');
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    characterElement.textContent = data.name;
+  });
